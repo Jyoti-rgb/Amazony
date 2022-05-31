6bf7e1ea191b6f471d048c0991f0c140bc59a93a
Joti mahor <jyotimahor6@gmail.com>
2022-05-31 08:12:39 +0530

    create api in backend for orderItems

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import productRouter from "./routers/productRouter.js";
 import userRouter from "./routers/userRouter.js";
+import orderRouter from "./routers/orderRouter.js";
 
 dotenv.config();
 
@@ -39,6 +40,7 @@ mongoose
 
 app.use("/api/users", userRouter);
 app.use("/api/products", productRouter);
+app.use("/api/orders", orderRouter);
 
 app.get("/", (req, res) => {
   res.send("Server is ready");
